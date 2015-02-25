990cf1c4f6d6840ba2981900b250b89395239cc9
Henry Tan <hkitannn@gmail.com>
2015-02-25 06:24:20 -0800

    Commit All

---
+++ iOSMidterm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSMidterm
+//
+//  Created by 킷탄 on 2015-02-25.
+//  Copyright (c) 2015 Team404. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
